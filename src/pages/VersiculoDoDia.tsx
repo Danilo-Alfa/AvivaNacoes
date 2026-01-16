86971afb713dd3ccb4bfa3950349bd6a8eb5aecf
Danilo Alfa <dalfahenrique@gmail.com>
2026-01-15 23:02:45 -0300

    feat: correção nas aspas do versiculo no celular?

--- src/pages/VersiculoDoDia.tsx
+++ src/pages/VersiculoDoDia.tsx
@@ -209,8 +209,8 @@ export default function VersiculoDoDia() {
                 onClick={() => versiculo.url_imagem ? setImagemTelaCheia(versiculo.url_imagem) : window.open(versiculo.url_post, "_blank")}
               >
                 {/* Aspas decorativas */}
-                <div className="absolute -top-1 -right-1 sm:top-2 sm:right-7 text-primary/10 pointer-events-none">
-                  <svg className="w-14 h-14 sm:w-16 sm:h-16" viewBox="0 0 24 24" fill="currentColor">
+                <div className="absolute top-3 right-6 sm:top-2 sm:right-6 text-primary/10 pointer-events-none">
+                  <svg className="w-12 h-12 sm:w-16 sm:h-16" viewBox="0 0 24 24" fill="currentColor">
                     <path d="M14.017 21v-7.391c0-5.704 3.731-9.57 8.983-10.609l.995 2.151c-2.432.917-3.995 3.638-3.995 5.849h4v10h-9.983zm-14.017 0v-7.391c0-5.704 3.748-9.57 9-10.609l.996 2.151c-2.433.917-3.996 3.638-3.996 5.849h3.983v10h-9.983z"/>
                   </svg>
                 </div>
